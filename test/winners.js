a3eee82048798315806136a48a5b535b52e15088
Marshall T. Rose <mrose17@gmail.com>
2018-03-19 20:27:01 -0700

    2.0.14: handle pinned publishers before statistical voting (#24)
    * 2.0.14: handle pinned publishers before statistical voting
    
    * Enhancements from review.
    
    * Comment from review
    
    * Fix rounding error found by @riastradh-brave
    
    Good catch!
    
    * Shuffle the input set
    
    to “even things out”
    
    * Add @mrose17's trivial test case.
    
    From https://github.com/brave-intl/bat-publisher/pull/24.
    
    * Appease linter's quotidien demands.
    
    * Add an unpinned site to the trivial test.
    
    * Test edge case.

---
+++ test/winners.js
@@ -0,0 +1,18 @@
+const tap = require('tap')
+const _ = require('underscore')
+
+const {Synopsis} = require('../index.js')
+
+const weights = [
+  { publisher: '10%', weight: 0.10, pinPercentage: 10 },
+  { publisher: '20%', weight: 0.20, pinPercentage: 20 },
+  { publisher: 'nada', weight: 0.47, pinPercentage: 0 },
+  { publisher: '30%', weight: 0.30, pinPercentage: 30 },
+  { publisher: '25%', weight: 0.25, pinPercentage: 25 },
+  { publisher: '15%', weight: 0.15, pinPercentage: 15 }
+]
+
+tap.same(Synopsis.prototype.winners(0, weights), [])
+
+tap.same(_.sortBy(Synopsis.prototype.winners(10, weights)),
+  ['10%', '15%', '15%', '20%', '20%', '25%', '25%', '30%', '30%', '30%'])
