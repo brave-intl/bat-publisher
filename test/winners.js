047e9627c53a551aa6b28d27269e3fa8a959a7de
ryanml <rlanese@asu.edu>
2018-06-15 15:13:53 -0700

    Fixing infinite loop/crash when no result sets have a pinPercentage

--- test/winners.js
+++ test/winners.js
@@ -25,4 +25,64 @@ weights = [
   { publisher: '17%', weight: 0.17 }
 ]
 tap.same(_.sortBy(Synopsis.prototype.winners(10, weights)),
-         [ '05%', '10%', '17%', '17%', '17%', '20%', '20%', '30%', '30%', '30%' ])
+  [ '05%', '10%', '17%', '17%', '17%', '20%', '20%', '30%', '30%', '30%' ])
+
+tap.same([], Synopsis.prototype.winners(92,
+  [
+    { publisher: 'brave.com', pinPercentage: undefined, weight: 0 }
+  ]
+))
+
+tap.same([], Synopsis.prototype.winners(92,
+  [
+    { publisher: 'brave.com', pinPercentage: undefined, weight: 0 },
+    { publisher: 'clifton.io', pinPercentage: undefined, weight: 0 }
+  ]
+))
+
+tap.same([], Synopsis.prototype.winners(92,
+  [
+    { publisher: 'brave.com', pinPercentage: undefined, weight: 0 },
+    { publisher: 'clifton.io', pinPercentage: undefined, weight: 0 },
+    { publisher: 'brianbondy.com', pinPercentage: null, weight: 0 }
+  ]
+))
+
+weights = [
+  { publisher: '30%', weight: 0.30, pinPercentage: 30 },
+  { publisher: '20%', weight: 0.20, pinPercentage: 20 },
+  { publisher: '10%', weight: 0.10, pinPercentage: 10 },
+  { publisher: '05%', weight: 0.05, pinPercentage: 5 },
+  { publisher: '17%', weight: 0.17 },
+  { publisher: 'brave.com', pinPercentage: undefined, weight: 0 },
+  { publisher: 'clifton.io', pinPercentage: undefined, weight: 0 }
+]
+
+tap.same(_.sortBy(Synopsis.prototype.winners(10, weights)),
+  [ '05%', '10%', '17%', '17%', '17%', '20%', '20%', '30%', '30%', '30%' ])
+
+weights = [
+  { publisher: '30%', weight: 0.30, pinPercentage: 30 },
+  { publisher: '20%', weight: 0.20, pinPercentage: 20 },
+  { publisher: '10%', weight: 0.10, pinPercentage: 10 },
+  { publisher: '05%', weight: 0.05, pinPercentage: 5 },
+  { publisher: '17%', weight: 0.17 },
+  { publisher: 'brave.com', weight: 0 },
+  { publisher: 'clifton.io', weight: 0 }
+]
+
+tap.same(_.sortBy(Synopsis.prototype.winners(10, weights)),
+  [ '05%', '10%', '17%', '17%', '17%', '20%', '20%', '30%', '30%', '30%' ])
+
+weights = [
+  { publisher: '30%', weight: 0.30, pinPercentage: 30 },
+  { publisher: '20%', weight: 0.20, pinPercentage: 20 },
+  { publisher: '10%', weight: 0.10, pinPercentage: 10 },
+  { publisher: '05%', weight: 0.05, pinPercentage: 5 },
+  { publisher: '17%', weight: 0.17 },
+  { publisher: 'brave.com' },
+  { publisher: 'clifton.io', weight: undefined }
+]
+
+tap.same(_.sortBy(Synopsis.prototype.winners(10, weights)),
+  [ '05%', '10%', '17%', '17%', '17%', '20%', '20%', '30%', '30%', '30%' ])
